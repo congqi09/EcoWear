4a2228b47de60c0c1c486d84299fe46581712f6c
Cong Qi <qi.co@northeastern.edu>
2024-04-03 08:47:18 +0900

    add items images

--- ecowear/mypage/urls.py
+++ ecowear/mypage/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path
 from mypage.views import getItemList, getAuctionList, getBidList, signup_view, login_view, home_view, user_profile, edit_user_profile
 
@@ -9,7 +8,6 @@ urlpatterns = [
     path('user_profile/<int:user_id>/', user_profile, name='user_profile'),
     path('user_profile/<int:user_id>/edit/', edit_user_profile, name='edit_user'),
     path('home/<int:user_id>/', home_view, name='home'),
-    path('admin/', admin.site.urls),
     path('item/', getItemList, name='item_list'),
     path('auction/', getAuctionList, name='auction_list'),
     path('bid/', getBidList, name='bid_list'),
